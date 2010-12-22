8170987d1ca995a14b94ca7eea68078f7e7240c3
Tomasz Przedzinski <tomasz.przediznski@cern.ch>
2010-12-22 18:27:39 +0000

    examples/Zmumum reverted to revision 225

--- examples/testing/Zmumu/SETUP.C
+++ examples/testing/Zmumu/SETUP.C
@@ -8,11 +8,9 @@
     // Setup histograms 
     int n_bins=120;
     double default_min_bin=0.0;
-        double default_max_bin=1.1;
-	//  double default_max_bin=12000.1;
-	//  double default_max_bin=100.;
+    double default_max_bin=1.1;
     Setup::SetHistogramDefaults(n_bins,default_min_bin,default_max_bin);
-        Setup::mass_scale_on=true;
+    Setup::mass_scale_on=true;
     
   
     // Description
@@ -22,15 +20,15 @@
     
     //Filter photons
     Setup::UserTreeAnalysis = "UserTreeAnalysis";
-    Setup::UTA_params[0]=0.01/91.187*100; //10 MeV
+    Setup::UTA_params[0]=0.01/91.187; //10 MeV
     // p_t threshold as fraction of particle energy in 
     // mothers frame 
     Setup::UTA_params[1]=2;
     Setup::UTA_params[2]=0.0;
     Setup::UTA_params[3]=1.0;
-    //    Setup::UTA_params[4]=22;
+    Setup::UTA_params[4]=22;
     
-    Setup::UTA_nparams=4;
+    Setup::UTA_nparams=5;
     
     Setup::SuppressDecay(22);
     Setup::SuppressDecay(23);
