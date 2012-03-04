123121f76d47b7f33e3fc67629397f30bd7f547c
Tomasz Przedzinski <tomasz.przediznski@cern.ch>
2012-03-04 16:27:40 +0000

    Fix: forgot 'index='

--- src/photosFortranInterfaces/PH_HEPEVT_Interface.cxx
+++ src/photosFortranInterfaces/PH_HEPEVT_Interface.cxx
@@ -149,7 +149,7 @@ void PH_HEPEVT_Interface::get(){
   //event record and modify their 4 momentum
   //4.03.2012: Fix to prevent kinematical trap in vertex of simultaneous:
   //           z-collinear and non-conservation pf E,p for dauthters of grandmothers
-  for(ph_hepevt_.jmohep[ph_hepevt_.nhep-1][0]; index < ph_hepevt_.nhep && index < (int) m_particle_list.size(); index++){
+  for(index=ph_hepevt_.jmohep[ph_hepevt_.nhep-1][0]; index < ph_hepevt_.nhep && index < (int) m_particle_list.size(); index++){
 
     PhotosParticle * particle = m_particle_list.at(index);
 
