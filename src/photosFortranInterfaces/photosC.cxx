0a53e6373d4a96f2b7d2028c892a15bf16191aa2
Zbigniew Was <z.was@cern.ch>
2013-06-10 09:45:44 +0000

    simple routine moved to C, issues of conventions for indices show up.

--- src/photosFortranInterfaces/photosC.cxx
+++ src/photosFortranInterfaces/photosC.cxx
@@ -13,3 +13,38 @@ bool F(int m, int i)
      && i != 1103 && i !=2103 && i !=2203 
      && i != 3103 && i !=3203 && i !=3303;
 }
+
+//----------------------------------------------------------------------
+//
+//    PHOEPS:   PHOeps vector product (normalized to unity)
+//
+//    Purpose:  calculates vector product, then normalizes its length.
+//              used to generate orthogonal vectors, i.e. to
+//              generate polarimetric vectors for photons.
+//
+//    Input Parameters:  VEC1,VEC2 - input 4-vectors
+//                                          
+//    Output Parameters: EPS - normalized 4-vector, orthogonal to
+//                             VEC1 and VEC2
+//
+//    Author(s):  Z. Was, P.Golonka               Created at:  19/01/05
+//                                                Last Update: 10/06/13
+//
+//----------------------------------------------------------------------
+
+void PHOEPS(double vec1[4], double vec2[4], double eps[4]){
+  double xn;
+  int j=1;  // convention of indices of Riemann space must be preserved.
+
+  eps[1-j]=vec1[2-j]*vec2[3-j] - vec1[3-j]*vec2[2-j];
+  eps[2-j]=vec1[3-j]*vec2[1-j] - vec1[1-j]*vec2[3-j];      
+  eps[3-j]=vec1[1-j]*vec2[2-j] - vec1[2-j]*vec2[1-j];
+  eps[4-j]=0D0;
+      
+  xn=SQRT( eps[1-j]**2 +eps[2-j]**2 +eps[3-j]**2);
+      
+  eps[1-j]=eps[1-j]/xn;
+  eps[2-j]=eps[2-j]/xn;
+  eps[3-j]=eps[3-j]/xn;
+
+}
--- src/photosFortranInterfaces/wrappers.cxx
+++ src/photosFortranInterfaces/wrappers.cxx
@@ -7,3 +7,10 @@ extern "C" bool f_(int *IDABS)
 {
   return F(0,*IDABS);
 }
+
+extern void PHOEPS(double vec1[4], double vec2[4], double eps[4]);
+
+extern "C" void phoeps_(double *VEC1[4], double *VEC2[4], double *EPS[4])
+{
+  return PHOEPS(*VEC1,*VEC2,*EPS);
+}
