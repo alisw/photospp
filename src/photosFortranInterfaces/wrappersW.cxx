04b1e6f3c064e95982b2ff4df35cb8cd674ba488
Zbigniew Was <z.was@cern.ch>
2013-06-13 17:11:16 +0000

    commony na struct

---
+++ src/photosFortranInterfaces/wrappersW.cxx
@@ -0,0 +1,8 @@
+extern "C" struct{
+  // COMMON /Kleiss_Stirling/spV,bet
+  double spV,bet
+} kleiss_stirling_;
+extern "C" struct{
+  // COMMON /mc_parameters/pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN
+  double pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN
+} mc_parameters_;
