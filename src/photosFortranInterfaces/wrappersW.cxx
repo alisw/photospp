d3b0387333bf0d18ee87ff356d4bec78f807e74c
Zbigniew Was <z.was@cern.ch>
2013-06-13 17:57:26 +0000

    attempt to use double complex and struct in formula friendly way

--- src/photosFortranInterfaces/wrappersW.cxx
+++ src/photosFortranInterfaces/wrappersW.cxx
@@ -6,3 +6,11 @@ extern "C" struct{
   // COMMON /mc_parameters/pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN
   double pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN
 } mc_parameters_;
+
+
+extern double complex WDecayEikonalKS_1ph(double p3[4],double p1[4],double p2[4],int s);
+
+extern "C" double complex WDecayEikonalKS_1ph_(double p3[4],double p1[4],double p2[4],int s)
+{
+  return WDecayEikonalKS_1ph(p3,p1,p2,s);
+}
