31c74ecd37359dcb0de98aba58958df919996d59
Zbigniew Was <z.was@cern.ch>
2013-06-14 07:15:06 +0000

    a bit of adaptation for less of modification of formulas on intermediate steps

--- src/photosFortranInterfaces/forW-MEc.cxx
+++ src/photosFortranInterfaces/forW-MEc.cxx
@@ -31,25 +31,29 @@ complex<double> WDecayEikonalKS_1ph(double p3[4],double p1[4],double p2[4],doubl
   double scalProd1,scalProd2,scalProd3;
   complex<double> wdecayeikonalks_1ph,BSoft1,BSoft2;  
   
-  // COMMON /mc_paraneters/pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN    
-  double &pi     = mc_parameters_.pi;
-  double &alphaI = mc_parameters_.alphaI;
-  double &qb     = mc_parameters_.qb;
-  double &mf1    = mc_parameters_.mf1;
-  double &mf2    = mc_parameters_.mf2;
-  double &qf1    = mc_parameters_.qf1;
-  double &qf2    = mc_parameters_.qf2;
+  // COMMON /mc_paraneters/pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN 
+  // temporary solution these will be global variables of the class   
+  double pi     = mc_parameters_.pi;
+  double alphaI = mc_parameters_.alphaI;
+  double qb     = mc_parameters_.qb;
+  double mf1    = mc_parameters_.mf1;
+  double mf2    = mc_parameters_.mf2;
+  double qf1    = mc_parameters_.qf1;
+  double qf2    = mc_parameters_.qf2;
+  // end of temporary solution
 
   scalProd1 = p1[0]*k[0]-p1[1]*k[1]-p1[2]*k[2]-p1[3]*k[3];
   scalProd2 = p2[0]*k[0]-p2[1]*k[1]-p2[2]*k[2]-p2[3]*k[3];
   scalProd3 = p3[0]*k[0]-p3[1]*k[1]-p3[2]*k[2]-p3[3]*k[3];
 
-  BSoft1  = bsfactor_(&s,k,p1,&mf1);
-  BSoft2  = bsfactor_(&s,k,p2,&mf2);
+  // to be activated once BsFactor is rewritten to C
+  //  BSoft1  = BsFactor(&s,k,p1,mf1);
+  //  BSoft2  = BsFactor($s,k,p2,mf2);
+  BSoft1  = bsfactor_(&s,k,p1,mf1);
+  BSoft2  = bsfactor_(&s,k,p2,mf2);
  
-  wdecayeikonalks_1ph = 
-        sqrt(pi/alphaI)*(-(qf1/scalProd1+qb/scalProd3)*BSoft1   
-                         +(qf2/scalProd2-qb/scalProd3)*BSoft2);
+  //WDecayEikonalKS_1ph =   
+   return sqrt(pi/alphaI)*(-(qf1/scalProd1+qb/scalProd3)*BSoft1   
+                           +(qf2/scalProd2-qb/scalProd3)*BSoft2);
 
-  return wdecayeikonalks_1ph;
 }
