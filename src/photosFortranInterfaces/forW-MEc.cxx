89fff49c716aaffca4c5bb7d868da6c4bec37d3d
Zbigniew Was <z.was@cern.ch>
2013-06-14 12:56:21 +0000

    another 3 routines from W ME moved to C

--- src/photosFortranInterfaces/forW-MEc.cxx
+++ src/photosFortranInterfaces/forW-MEc.cxx
@@ -11,7 +11,129 @@ extern "C" struct{
   double pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN;
 } mc_parameters_;
 
-//=====================================================================                 
+
+
+
+
+//////////////////////////////////////////////////////////////////
+//         small s_{+,-}(p1,p2) for massless case:              //
+//                 p1^2 = p2^2 = 0                              // 
+//                                                              //
+//     k0(0) = 1.d0                                             //
+//     k0(1) = 1.d0                                             //
+//     k0(2) = 0.d0  Kleisse_Stirling k0 points to X-axis       // 
+//     k0(3) = 0.d0                                             //
+//                                                              //
+//////////////////////////////////////////////////////////////////
+complex<double> InProd_zero(double p1[4],int l1,double p2[4],int l2){
+
+  int i; 
+  double  forSqrt1,forSqrt2,sqrt1,sqrt2;
+  complex<double>    Dcmplx;
+  static complex<double>    i_=(0.0,1.0);
+  bool           equal;
+
+
+
+  equal = true;  
+  for (int i = 0; i < 4; i++){
+ 
+    if (p1[i]!=p2[i])  equal = equal && false ;
+  }               
+
+
+  if ( (l1==l2) || equal ) return (0.0,0.0);
+
+ 
+  else if ( (l1==+1) && (l2==-1) ){
+
+    forSqrt1 = (p1[0]-p1[1])/(p2[0]-p2[1]);
+    forSqrt2 = 1.0/forSqrt1;
+    sqrt1    = sqrt(forSqrt2);
+    sqrt2    = sqrt(forSqrt1);
+
+    return (p1[2]+i_*p1[3])*sqrt1 -
+	   (p2[2]+i_*p2[3])*sqrt2 ;
+  }
+  else if ( (l1==-1) && (l2==+1) ){
+
+    forSqrt1 = (p1[0]-p1[1])/(p2[0]-p2[1]);
+    forSqrt2 = 1.0/forSqrt1;
+    sqrt1    = sqrt(forSqrt2);
+    sqrt2    = sqrt(forSqrt1);
+
+    return (p2(2)-i_*p2(3))*sqrt2 -
+           (p1(2)-i_*p1(3))*sqrt1 ;
+  }
+  else{
+                 
+
+    cout << " "<<endl;             
+    cout << " ERROR IN InProd_zero:"<<endl;
+    cout << "   WRONG VALUES FOR l1,l2: l1,l2 = -1,+1 "<<endl;
+    cout << " "  <<endl;           
+    exit(0);
+  }
+}
+
+double InSqrt(double p[4],double q[4]){
+            
+  return sqrt( (p[0]-p[1]) / (q[0]-q[1]) );
+}
+    
+
+
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//  this is small B_{s}(k,p) function when TrMartix is diaagonal!! //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+  complex<double>  BsFactor(int s,double k[4],double p[4],double m){
+    double forSqrt1,sqrt1;
+    complex<double>  InProd_zero,inPr1;
+  
+  // COMMON /mc_paraneters/pi,sw,cw,alphaI,qb,mb,mf1,mf2,qf1,qf2,vf,af,mcLUN 
+  // temporary solution these will be global variables of the class   
+  double pi     = mc_parameters_.pi;
+  double alphaI = mc_parameters_.alphaI;
+  double qb     = mc_parameters_.qb;
+  double mf1    = mc_parameters_.mf1;
+  double mf2    = mc_parameters_.mf2;
+  double qf1    = mc_parameters_.qf1;
+  double qf2    = mc_parameters_.qf2;
+  // end of temporary solution
+
+  if ( s==1 ){ 
+
+    inPr1    = InProd_zero(k,+1,p,-1);
+    forSqrt1 = (p[0]-p[1])/(k[0]-k[1]);
+    sqrt1    = Dsqrt(2.0*forSqrt1);  
+    //BsFactor = 
+    return inPr1*sqrt1;
+  }
+
+  else if ( s==-1 ){
+
+    inPr1    = InProd_zero(k,-1,p,+1);
+    forSqrt1 = (p[0]-p[1])/(k[0]-k[1]);
+    sqrt1    = Dsqrt(2.0*forSqrt1); 
+    //BsFactor = 
+    return inPr1*sqrt1;
+  }
+  else{
+
+    cout << " "<<endl;             
+    cout << " ERROR IN BsFactor: "<<endl;
+    cout << "       WRONG VALUES FOR s : s = -1,+1"<<endl;
+    cout << " "  <<endl;           
+    exit(0);
+  }
+
+
+
+
+
+//====================================================================== 
 //     
 // Eikonal factor of decay W->l_1+l_2+\gamma in terms of K&S objects !
 // 
@@ -23,9 +145,6 @@ extern "C" struct{
 //   q - charge
 //    
 //======================================================================
-
-extern "C" complex<double> bsfactor_(int *s, double *k, double *p, double *m);
-
 complex<double> WDecayEikonalKS_1ph(double p3[4],double p1[4],double p2[4],double k[4],int s){
 
   double scalProd1,scalProd2,scalProd3;
@@ -46,11 +165,9 @@ complex<double> WDecayEikonalKS_1ph(double p3[4],double p1[4],double p2[4],doubl
   scalProd2 = p2[0]*k[0]-p2[1]*k[1]-p2[2]*k[2]-p2[3]*k[3];
   scalProd3 = p3[0]*k[0]-p3[1]*k[1]-p3[2]*k[2]-p3[3]*k[3];
 
-  // to be activated once BsFactor is rewritten to C
-  //  BSoft1  = BsFactor(&s,k,p1,mf1);
-  //  BSoft2  = BsFactor($s,k,p2,mf2);
-  BSoft1  = bsfactor_(&s,k,p1,mf1);
-  BSoft2  = bsfactor_(&s,k,p2,mf2);
+
+    BSoft1  = BsFactor(&s,k,p1,mf1);
+    BSoft2  = BsFactor(&s,k,p2,mf2);
  
   //WDecayEikonalKS_1ph =   
    return sqrt(pi/alphaI)*(-(qf1/scalProd1+qb/scalProd3)*BSoft1   
--- src/photosFortranInterfaces/wrappersW.cxx
+++ src/photosFortranInterfaces/wrappersW.cxx
@@ -1,6 +1,28 @@
 #include <complex>
 using std::complex;
 
+extern complex<double> InProd_zero(double p1[4],int l1,double p2[4],int l2);
+
+extern "C" complex<double> inprod_zero_(double p1[4],int l1,double p2[4],int l2)
+{
+  return InProd_zero(p1,l1,p2,l2);
+}
+
+double InSqrt(double p[4],double q[4]);
+
+extern "C" double insqrt_(double p[4],double q[4])
+{
+  return InSqrt(p,q);
+}
+
+
+extern complex<double> BsFactor(int s,double k[4],double p[4],double m);
+
+extern "C" complex<double> bsfactor_(int s,double k[4],double p[4],double m)
+{
+  return BsFactor(s,k,p,m);
+}
+
 extern complex<double> WDecayEikonalKS_1ph(double p3[4],double p1[4],double p2[4],double k[4],int s);
 
 extern "C" complex<double> wdecayeikonalks_1ph_(double p3[4],double p1[4],double p2[4],double k[4],int *s)
