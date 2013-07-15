0e88a880b8f7c468f204b0ff2a43b04a0ce68ed6
Zbigniew Was <z.was@cern.ch>
2013-07-15 15:00:18 +0000

    step toward utilities?

--- src/utilities/PhotosUtilities.h
+++ src/utilities/PhotosUtilities.h
@@ -1,6 +1,9 @@
 #ifndef _PhotosUtilities_h_included_
 #define _PhotosUtilities_h_included_
-
+#include <cmath>
+//#include <iostream>
+using std::cout;
+using std::endl;
 /**
  * @class PhotosUtilities
  *
@@ -16,9 +19,31 @@ namespace Photospp
 
 namespace PhotosUtilities
 {
+  /** PHOton radiation in decays calculation of TRIangle fie */
+  double PHOTRI(double A,double B,double C);
 
-	/** PHOton radiation in decays BOost routine '3' */
-	void PHOBO3(double ANGLE,double PVEC[4]);
+  /** PHOton radiation in decays Calculate ANgle from X and Y */
+  double PHOAN1(double X,double Y);
+
+  /** PHOton radiation in decays Calculate ANgle from X and Y equiv to PHOAN1 */
+  double PHOAN2(double X,double Y);
+
+  /** PHOton radiation in decays ROtation routine around 2-nd axis */
+  void PHORO2(double ANGLE,double PVEC[4]);
+
+
+  /** PHOton radiation in decays ROtation routine around 3-rd axis */
+  void PHORO3(double ANGLE,double PVEC[4]);
+
+  /** Boot to-from restr frame of PBOOS1 */
+  void PHOB(int MODE,double PBOOS1[4],double vec[4]);
+
+  /** PHOton radiation in decays BOost routine along arbitrary axis axis */
+  void bostdq(int mode,double qq[4],double pp[4],double r[4]);
+
+
+  /** PHOton radiation in decays BOost routine along 3-rd axis */
+  void PHOBO3(double ANGLE,double PVEC[4]);
 
 } // namespace PhotosUtilities
 
