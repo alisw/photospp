c7da347fd91b875de054fc17cc4ecc860b75263d
Tomasz Przedzinski <tomasz.przediznski@cern.ch>
2013-09-16 10:05:30 +0000

    HEPEVT example updated

--- examples/photos_hepevt_example_interface.cxx
+++ examples/photos_hepevt_example_interface.cxx
@@ -29,4 +29,38 @@ extern "C" {
 
     delete event;
   }
+
+  void photos_process_particle_(int *i)
+  {
+    PhotosHEPEVTEvent *event = new PhotosHEPEVTEvent();
+
+    PhotosHEPEVTEvent::read_event_from_HEPEVT(event);
+    //event->print();
+
+    PhotosParticle *p = event->getParticle(*i - 1);
+
+    Photos::processParticle(p);
+    //event->print();
+
+    PhotosHEPEVTEvent::write_event_to_HEPEVT(event);
+
+    delete event;
+  }
+
+  void photos_process_branch_(int *i)
+  {
+    PhotosHEPEVTEvent *event = new PhotosHEPEVTEvent();
+
+    PhotosHEPEVTEvent::read_event_from_HEPEVT(event);
+    //event->print();
+
+    PhotosParticle *p = event->getParticle(*i - 1);
+
+    Photos::processBranch(p);
+    //event->print();
+
+    PhotosHEPEVTEvent::write_event_to_HEPEVT(event);
+
+    delete event;
+  }
 }
