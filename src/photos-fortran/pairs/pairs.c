57eb097c892ee34e7f39b2ad06bf13efeafa9d5f
Zbigniew Was <z.was@cern.ch>
2014-12-13 10:33:04 +0000

    little progress, but nonetheless. Learning at the time old code.

--- src/photos-fortran/pairs/pairs.c
+++ src/photos-fortran/pairs/pairs.c
@@ -1,9 +1,65 @@
 #include <cmath>
 #include <stdio.h>
 #include <stdlib.h>  
+extern "C" double angfi_(double *pX,double *pY){                                               
+  const double &X = *pX;
+  const double &Y = *pY;
+  double THE;
+  const double PI=3.141592653589793238462643;
+
+  if(X==0.0 && Y==0.0) return 0.0;           
+   if(fabs(Y) < fabs(X)){                                         
+    THE=atan(fabs(Y/X));                                              
+    if(X<=0.0) THE=PI-THE;}                                         
+  else{                                                              
+    THE=acos(X/sqrt(X*X +Y*Y));                                     
+  }
+   if(Y<0.0) THE=2*PI-THE;
+   return THE;
+}
+
+extern "C" double angxy_(double *pX,double *pY){                                               
+  const double &X = *pX;
+  const double &Y = *pY;
+  double THE;
+  const double PI=3.141592653589793238462643;
+
+  if(X==0.0 && Y==0.0) return 0.0;
+
+  if(fabs(Y) < fabs(X)){                                         
+    THE=atan(fabs(Y/X));                                              
+    if(X<=0.0) THE=PI-THE;}                                         
+  else{                                                              
+    THE=acos(X/sqrt(X*X +Y*Y));                                     
+  }                                       
+  return THE;                                                         
+}
+
+extern "C" void  bostd3_(double *pEXE,double PVEC[4],double QVEC[4]){
+  // ----------------------------------------------------------------------
+  // BOOST ALONG Z AXIS, EXE=EXP(ETA), ETA= HIPERBOLIC VELOCITY.
+  //
+  //     USED BY : KORALZ RADKOR
+  /// ----------------------------------------------------------------------
+  const double EXE = *pEXE;
+  int j=1;  // convention of indices of Riemann space must be preserved.
+  double RPL,RMI,QPL,QMI;
+  double RVEC[4];
 
 
-
+  RVEC[1-j]=PVEC[1-j];
+  RVEC[2-j]=PVEC[2-j];
+  RVEC[3-j]=PVEC[3-j];
+  RVEC[4-j]=PVEC[4-j];
+  RPL=RVEC[4-j]+RVEC[3-j];
+  RMI=RVEC[4-j]-RVEC[3-j];
+  QPL=RPL*EXE;
+  QMI=RMI/EXE;
+  QVEC[1-j]=RVEC[1-j];
+  QVEC[2-j]=RVEC[2-j];
+  QVEC[3-j]=(QPL-QMI)/2;
+  QVEC[4-j]=(QPL+QMI)/2;
+}
 
 // after investigations PHORO3 of PhotosUtilities.cxx will be used instead
 // but it must be checked first if it works
@@ -46,4 +102,51 @@ extern "C" void   rotod2_(double *PH1,double PVEC[4],double QVEC[4]){
   //   printf ("%15.12f %15.12f %15.12f %15.12f \n",QVEC[0],QVEC[1],QVEC[2],QVEC[3]);
   // exit(-1);
 }
+
+
+extern "C" void   lortra_(int *pKEY,double *pPRM,double PNEUTR[4],double PNU[4],double PAA[4],double PP[4],double PE[4]){
+  // --------------------------------------------------------------------- 
+  // THIS ROUTINE PERFORMS LORENTZ TRANSFORMATION ON MANY 4-VECTORS        
+  // KEY   =1    BOOST    ALONG   3RD AXIS                                 
+  //       =2    ROTATION AROUND 2ND AXIS                                  
+  //       =3    ROTATION AROUND 3RD AXIS                                 
+  // PRM         TRANSFORMATION PARAMETER - ANGLE OR EXP(HIPERANGLE).     
+  //                                                                      
+  //    called by : RADCOR                                                
+  // --------------------------------------------------------------------- 
+  const int &KEY = *pKEY;
+  double PRM = *pPRM;                                                                       
+  if(KEY==1){                                           
+    bostd3_(&PRM,PNEUTR,PNEUTR);                                 
+    bostd3_(&PRM,PNU ,PNU );                                     
+    bostd3_(&PRM,PAA ,PAA );                                     
+    bostd3_(&PRM,PE ,PE );                                     
+    bostd3_(&PRM,PP ,PP );
+  }                                     
+  else if (KEY==2){                                           
+    rotod2_(&PRM,PNEUTR,PNEUTR);                                 
+    rotod2_(&PRM,PNU ,PNU );                                     
+    rotod2_(&PRM,PAA ,PAA );                                    
+    rotod2_(&PRM,PE  ,PE  );                                     
+    rotod2_(&PRM,PP  ,PP );
+  }                                    
+  else if (KEY==3){                                          
+    rotod3_(&PRM,PNEUTR,PNEUTR);                                
+    rotod3_(&PRM,PNU ,PNU );                                     
+    rotod3_(&PRM,PAA ,PAA );                                    
+    rotod3_(&PRM,PE  ,PE  );                                     
+    rotod3_(&PRM,PP  ,PP );
+  }                                    
+  else{                                                            
+    printf (" STOP IN LOTRA. WRONG KEYTRA"); 
+    exit(-1);
+  }
+}
+
+extern "C" double amast_(double VEC[4]){
+  int i=1;  // convention of indices of Riemann space must be preserved
+  double ama= VEC[4-i]*VEC[4-i]-VEC[1-i]*VEC[1-i]-VEC[2-i]*VEC[2-i]-VEC[3-i]*VEC[3-i];
+  ama=sqrt(fabs(ama));
+  return ama;
+} 
 
