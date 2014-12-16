c9877b3b86ad99b3da59d96dd5c3b4160146c83e
Zbigniew Was <z.was@cern.ch>
2014-12-16 12:48:18 +0000

    cleaning; main step: removal of xxx_( routines

--- src/photos-fortran/pairs/pairs.c
+++ src/photos-fortran/pairs/pairs.c
@@ -10,23 +10,6 @@
   }
    // 
 extern "C" void varran_( double RRR[], int *N);
-
-extern "C" double angfi_(double *pX,double *pY){                                               
-  const double &X = *pX;
-  const double &Y = *pY;
-  double THE;
-  const double PI=3.141592653589793238462643;
-
-  if(X==0.0 && Y==0.0) return 0.0;           
-   if(fabs(Y) < fabs(X)){                                         
-    THE=atan(fabs(Y/X));                                              
-    if(X<=0.0) THE=PI-THE;}                                         
-  else{                                                              
-    THE=acos(X/sqrt(X*X +Y*Y));                                     
-  }
-   if(Y<0.0) THE=2*PI-THE;
-   return THE;
-}
 extern "C" double angfi(double X,double Y){                                               
   double THE;
   const double PI=3.141592653589793238462643;
@@ -40,23 +23,6 @@ extern "C" double angfi(double X,double Y){
   }
    if(Y<0.0) THE=2*PI-THE;
    return THE;
-}
-
-extern "C" double angxy_(double *pX,double *pY){                                               
-  const double &X = *pX;
-  const double &Y = *pY;
-  double THE;
-  const double PI=3.141592653589793238462643;
-
-  if(X==0.0 && Y==0.0) return 0.0;
-
-  if(fabs(Y) < fabs(X)){                                         
-    THE=atan(fabs(Y/X));                                              
-    if(X<=0.0) THE=PI-THE;}                                         
-  else{                                                              
-    THE=acos(X/sqrt(X*X +Y*Y));                                     
-  }                                       
-  return THE;                                                         
 }
 
 extern "C" double angxy(double X,double Y){                                               
@@ -74,31 +40,6 @@ extern "C" double angxy(double X,double Y){
   return THE;                                                         
 }
 
-extern "C" void  bostd3_(double *pEXE,double PVEC[4],double QVEC[4]){
-  // ----------------------------------------------------------------------
-  // BOOST ALONG Z AXIS, EXE=EXP(ETA), ETA= HIPERBOLIC VELOCITY.
-  //
-  //     USED BY : KORALZ RADKOR
-  /// ----------------------------------------------------------------------
-  const double EXE = *pEXE;
-  int j=1;  // convention of indices of Riemann space must be preserved.
-  double RPL,RMI,QPL,QMI;
-  double RVEC[4];
-
-
-  RVEC[1-j]=PVEC[1-j];
-  RVEC[2-j]=PVEC[2-j];
-  RVEC[3-j]=PVEC[3-j];
-  RVEC[4-j]=PVEC[4-j];
-  RPL=RVEC[4-j]+RVEC[3-j];
-  RMI=RVEC[4-j]-RVEC[3-j];
-  QPL=RPL*EXE;
-  QMI=RMI/EXE;
-  QVEC[1-j]=RVEC[1-j];
-  QVEC[2-j]=RVEC[2-j];
-  QVEC[3-j]=(QPL-QMI)/2;
-  QVEC[4-j]=(QPL+QMI)/2;
-}
 extern "C" void  bostd3(double EXE,double PVEC[4],double QVEC[4]){
   // ----------------------------------------------------------------------
   // BOOST ALONG Z AXIS, EXE=EXP(ETA), ETA= HIPERBOLIC VELOCITY.
@@ -126,46 +67,6 @@ extern "C" void  bostd3(double EXE,double PVEC[4],double QVEC[4]){
 
 // after investigations PHORO3 of PhotosUtilities.cxx will be used instead
 // but it must be checked first if it works
-
-extern "C" void rotod3_(double *pANGLE,double PVEC[4],double QVEC[4]){
-  const double &ANGLE = *pANGLE;
- 
-  int j=1;  // convention of indices of Riemann space must be preserved.
-  double CS,SN;
-  //  printf ("%5.2f\n",cos(ANGLE));
-  CS=cos(ANGLE)*PVEC[1-j]-sin(ANGLE)*PVEC[2-j];
-  SN=sin(ANGLE)*PVEC[1-j]+cos(ANGLE)*PVEC[2-j];
-
-  QVEC[1-j]=CS;
-  QVEC[2-j]=SN;
-  QVEC[3-j]=PVEC[3-j];
-  QVEC[4-j]=PVEC[4-j];
-}
-
-
-
-extern "C" void   rotod2_(double *PH1,double PVEC[4],double QVEC[4]){
-  const double &PHI = *PH1;
-  double RVEC[4];
-  int j=1;  // convention of indices of Riemann space must be preserved.
-  double CS,SN;
-
-  CS=cos(PHI);
-  SN=sin(PHI);
-
-  RVEC[1-j]=PVEC[1-j];
-  RVEC[2-j]=PVEC[2-j];
-  RVEC[3-j]=PVEC[3-j];
-  RVEC[4-j]=PVEC[4-j];
-
-  QVEC[1-j]= CS*RVEC[1-j]+SN*RVEC[3-j];
-  QVEC[2-j]=RVEC[2-j];
-  QVEC[3-j]=-SN*RVEC[1-j]+CS*RVEC[3-j];
-  QVEC[4-j]=RVEC[4-j];
-  //   printf ("%15.12f %15.12f %15.12f %15.12f \n",QVEC[0],QVEC[1],QVEC[2],QVEC[3]);
-  // exit(-1);
-}
-
 
 extern "C" void rotod3(double ANGLE,double PVEC[4],double QVEC[4]){
 
@@ -206,46 +107,6 @@ extern "C" void   rotod2(double PHI,double PVEC[4],double QVEC[4]){
   // exit(-1);
 }
 
-
-extern "C" void   lortra_(int *pKEY,double *pPRM,double PNEUTR[4],double PNU[4],double PAA[4],double PP[4],double PE[4]){
-  // --------------------------------------------------------------------- 
-  // THIS ROUTINE PERFORMS LORENTZ TRANSFORMATION ON MANY 4-VECTORS        
-  // KEY   =1    BOOST    ALONG   3RD AXIS                                 
-  //       =2    ROTATION AROUND 2ND AXIS                                  
-  //       =3    ROTATION AROUND 3RD AXIS                                 
-  // PRM         TRANSFORMATION PARAMETER - ANGLE OR EXP(HIPERANGLE).     
-  //                                                                      
-  //    called by : RADCOR                                                
-  // --------------------------------------------------------------------- 
-  const int &KEY = *pKEY;
-  double PRM = *pPRM;                                                                       
-  if(KEY==1){                                           
-    bostd3_(&PRM,PNEUTR,PNEUTR);                                 
-    bostd3_(&PRM,PNU ,PNU );                                     
-    bostd3_(&PRM,PAA ,PAA );                                     
-    bostd3_(&PRM,PE ,PE );                                     
-    bostd3_(&PRM,PP ,PP );
-  }                                     
-  else if (KEY==2){                                           
-    rotod2_(&PRM,PNEUTR,PNEUTR);                                 
-    rotod2_(&PRM,PNU ,PNU );                                     
-    rotod2_(&PRM,PAA ,PAA );                                    
-    rotod2_(&PRM,PE  ,PE  );                                     
-    rotod2_(&PRM,PP  ,PP );
-  }                                    
-  else if (KEY==3){                                          
-    rotod3_(&PRM,PNEUTR,PNEUTR);                                
-    rotod3_(&PRM,PNU ,PNU );                                     
-    rotod3_(&PRM,PAA ,PAA );                                    
-    rotod3_(&PRM,PE  ,PE  );                                     
-    rotod3_(&PRM,PP  ,PP );
-  }                                    
-  else{                                                            
-    printf (" STOP IN LOTRA. WRONG KEYTRA"); 
-    exit(-1);
-  }
-}
-
 extern "C" void   lortra(int KEY,double PRM,double PNEUTR[4],double PNU[4],double PAA[4],double PP[4],double PE[4]){
   // --------------------------------------------------------------------- 
   // THIS ROUTINE PERFORMS LORENTZ TRANSFORMATION ON MANY 4-VECTORS        
@@ -257,25 +118,25 @@ extern "C" void   lortra(int KEY,double PRM,double PNEUTR[4],double PNU[4],doubl
   //    called by : RADCOR                                                
   // --------------------------------------------------------------------- 
   if(KEY==1){                                           
-    bostd3_(&PRM,PNEUTR,PNEUTR);                                 
-    bostd3_(&PRM,PNU ,PNU );                                     
-    bostd3_(&PRM,PAA ,PAA );                                     
-    bostd3_(&PRM,PE ,PE );                                     
-    bostd3_(&PRM,PP ,PP );
+    bostd3(PRM,PNEUTR,PNEUTR);                                 
+    bostd3(PRM,PNU ,PNU );                                     
+    bostd3(PRM,PAA ,PAA );                                     
+    bostd3(PRM,PE ,PE );                                     
+    bostd3(PRM,PP ,PP );
   }                                     
   else if (KEY==2){                                           
-    rotod2_(&PRM,PNEUTR,PNEUTR);                                 
-    rotod2_(&PRM,PNU ,PNU );                                     
-    rotod2_(&PRM,PAA ,PAA );                                    
-    rotod2_(&PRM,PE  ,PE  );                                     
-    rotod2_(&PRM,PP  ,PP );
+    rotod2(PRM,PNEUTR,PNEUTR);                                 
+    rotod2(PRM,PNU ,PNU );                                     
+    rotod2(PRM,PAA ,PAA );                                    
+    rotod2(PRM,PE  ,PE  );                                     
+    rotod2(PRM,PP  ,PP );
   }                                    
   else if (KEY==3){                                          
-    rotod3_(&PRM,PNEUTR,PNEUTR);                                
-    rotod3_(&PRM,PNU ,PNU );                                     
-    rotod3_(&PRM,PAA ,PAA );                                    
-    rotod3_(&PRM,PE  ,PE  );                                     
-    rotod3_(&PRM,PP  ,PP );
+    rotod3(PRM,PNEUTR,PNEUTR);                                
+    rotod3(PRM,PNU ,PNU );                                     
+    rotod3(PRM,PAA ,PAA );                                    
+    rotod3(PRM,PE  ,PE  );                                     
+    rotod3(PRM,PP  ,PP );
   }                                    
   else{                                                            
     printf (" STOP IN LOTRA. WRONG KEYTRA"); 
@@ -283,34 +144,12 @@ extern "C" void   lortra(int KEY,double PRM,double PNEUTR[4],double PNU[4],doubl
   }
 }
 
-extern "C" double amast_(double VEC[4]){
+double amast(double VEC[4]){
   int i=1;  // convention of indices of Riemann space must be preserved
   double ama= VEC[4-i]*VEC[4-i]-VEC[1-i]*VEC[1-i]-VEC[2-i]*VEC[2-i]-VEC[3-i]*VEC[3-i];
   ama=sqrt(fabs(ama));
   return ama;
 } 
-
-extern "C" void spaj_(int *pKUDA,double P2[4],double Q2[4],double PP[4],double PE[4]){    
-  //     **********************     
-  // THIS PRINTS OUT FOUR MOMENTA OF PHOTONS 
-  // ON OUTPUT UNIT NOUT
-  const int &KUDA = *pKUDA;
-
-  double SUM[4];
-  const int KLUCZ=0;
-  if (KLUCZ==0) return;
-
-  printf (" %10i =====================SPAJ==================== \n", KUDA);
-  printf (" P2 %18.13f %18.13f %18.13f %18.13f \n",P2[0],P2[1],P2[2],P2[3]);
-  printf (" Q2 %18.13f %18.13f %18.13f %18.13f \n",Q2[0],Q2[1],Q2[2],Q2[3]);
-  printf (" PE %18.13f %18.13f %18.13f %18.13f \n",PE[0],PE[1],PE[2],PE[3]);
-  printf (" PP %18.13f %18.13f %18.13f %18.13f \n",PP[0],PP[1],PP[2],PP[3]);
- 
-  for( int k=0;k<=3;k++) SUM[k]=P2[k]+Q2[k]+PE[k]+PP[k];
-
-  printf ("SUM %18.13f %18.13f %18.13f %18.13f \n",SUM[0],SUM[1],SUM[2],SUM[3]);
-}
-
 
 extern "C" void spaj(int KUDA,double P2[4],double Q2[4],double PP[4],double PE[4]){    
   //     **********************     
@@ -332,8 +171,8 @@ extern "C" void spaj(int KUDA,double P2[4],double Q2[4],double PP[4],double PE[4
   printf ("SUM %18.13f %18.13f %18.13f %18.13f \n",SUM[0],SUM[1],SUM[2],SUM[3]);
 }
 
-extern "C" {
- extern struct { 
+//extern "C" {
+ extern struct PARKIN { 
    double fi0; // FI0
    double fi1; // FI1
    double fi2; // FI2
@@ -349,50 +188,52 @@ extern "C" {
    double bpar; // BPAR
    double bsta; // BSTA
    double bstb; // BSTB
-  } parkin_;
-}
+  } parkin;
+//}
+
+struct PARKIN parkin;
 
 extern "C" void partra_(int *pIBRAN,double PHOT[4]){
 
    const int &IBRAN = *pIBRAN;
 
    // particularily stupid way of introdicing minus for argument of the function.
-   double FI0 = -parkin_.fi0;
-   double FI1 = -parkin_.fi1;
-   double FI2 = -parkin_.fi2;
-   double FI3 = -parkin_.fi3;
-   double FI5 = -parkin_.fi5;
-   double TH0 = -parkin_.th0;
-   double TH1 = -parkin_.th1;
-   double TH3 = -parkin_.th3;
-   double TH4 = -parkin_.th4;
+   double FI0 = -parkin.fi0;
+   double FI1 = -parkin.fi1;
+   double FI2 = -parkin.fi2;
+   double FI3 = -parkin.fi3;
+   double FI5 = -parkin.fi5;
+   double TH0 = -parkin.th0;
+   double TH1 = -parkin.th1;
+   double TH3 = -parkin.th3;
+   double TH4 = -parkin.th4;
 
-   rotod3_(        &FI0,PHOT,PHOT); 
-   rotod2_(        &TH0,PHOT,PHOT);
-   bostd3_(&parkin_.bsta,PHOT,PHOT);
-   rotod3_(        &FI1,PHOT,PHOT);
-   rotod2_(        &TH1,PHOT,PHOT);
-   rotod3_(        &FI2,PHOT,PHOT);
+   rotod3(        FI0,PHOT,PHOT); 
+   rotod2(        TH0,PHOT,PHOT);
+   bostd3(parkin.bsta,PHOT,PHOT);
+   rotod3(        FI1,PHOT,PHOT);
+   rotod2(        TH1,PHOT,PHOT);
+   rotod3(        FI2,PHOT,PHOT);
 
    if(IBRAN==-1){
-     bostd3_(&parkin_.parneu,PHOT,PHOT);
+     bostd3(parkin.parneu,PHOT,PHOT);
    }
    else{
-     bostd3_(&parkin_.parch,PHOT,PHOT);
+     bostd3(parkin.parch,PHOT,PHOT);
    }
 
-   rotod3_(        &FI3,PHOT,PHOT);
-   rotod2_(        &TH3,PHOT,PHOT);
-   bostd3_(&parkin_.bpar,PHOT,PHOT);
-   rotod3_(&parkin_.fi4,PHOT,PHOT);
-   rotod2_(        &TH4,PHOT,PHOT);
-   rotod3_(        &FI5,PHOT,PHOT);
-   rotod3_(&parkin_.fi2,PHOT,PHOT);
-   rotod2_(&parkin_.th1,PHOT,PHOT); 
-   rotod3_(&parkin_.fi1,PHOT,PHOT);
-   bostd3_(&parkin_.bstb,PHOT,PHOT);
-   rotod2_(&parkin_.th0,PHOT,PHOT);
-   rotod3_(&parkin_.fi0,PHOT,PHOT);
+   rotod3(        FI3,PHOT,PHOT);
+   rotod2(        TH3,PHOT,PHOT);
+   bostd3(parkin.bpar,PHOT,PHOT);
+   rotod3(parkin.fi4,PHOT,PHOT);
+   rotod2(        TH4,PHOT,PHOT);
+   rotod3(        FI5,PHOT,PHOT);
+   rotod3(parkin.fi2,PHOT,PHOT);
+   rotod2(parkin.th1,PHOT,PHOT); 
+   rotod3(parkin.fi1,PHOT,PHOT);
+   bostd3(parkin.bstb,PHOT,PHOT);
+   rotod2(parkin.th0,PHOT,PHOT);
+   rotod3(parkin.fi0,PHOT,PHOT);
    
 }
 
@@ -401,21 +242,21 @@ extern "C" void trypar_(bool *pJESLI,double *pSTRENG,double PA[4],double PB[4],d
   bool &JESLI = *pJESLI;
   const double &STRENG = *pSTRENG;
   //      COMMON  /PARKIN/ 
-  double &FI0=parkin_.fi0;
-  double &FI1=parkin_.fi1;
-  double &FI2=parkin_.fi2;
-  double &FI3=parkin_.fi3;
-  double &FI4=parkin_.fi4;
-  double &FI5=parkin_.fi5;
-  double &TH0=parkin_.th0;
-  double &TH1=parkin_.th1;
-  double &TH3=parkin_.th3;
-  double &TH4=parkin_.th4;
-  double &PARNEU=parkin_.parneu;
-  double &PARCH=parkin_.parch;
-  double &BPAR=parkin_.bpar;
-  double &BSTA=parkin_.bsta;
-  double &BSTB=parkin_.bstb;
+  double &FI0=parkin.fi0;
+  double &FI1=parkin.fi1;
+  double &FI2=parkin.fi2;
+  double &FI3=parkin.fi3;
+  double &FI4=parkin.fi4;
+  double &FI5=parkin.fi5;
+  double &TH0=parkin.th0;
+  double &TH1=parkin.th1;
+  double &TH3=parkin.th3;
+  double &TH4=parkin.th4;
+  double &PARNEU=parkin.parneu;
+  double &PARCH=parkin.parch;
+  double &BPAR=parkin.bpar;
+  double &BSTA=parkin.bsta;
+  double &BSTB=parkin.bstb;
 
   double  PNEUTR[4],PAA[4],PHOT[4],PSUM[4];
   double VEC[4];                                                
@@ -462,8 +303,8 @@ extern "C" void trypar_(bool *pJESLI,double *pSTRENG,double PA[4],double PB[4],d
   BSTB=(PSUM[4-j]+PSUM[3-j])/sqrt(PSUM[4-j]*PSUM[4-j]-PSUM[3-j]*PSUM[3-j]);
   lortra(1,BSTA,PNEUTR,VEC,PAA,PP,PE);
   spaj(-1,PNEUTR,PAA,PP,PE);                                  
-  double AMNE=amast_(PNEUTR);                                              
-  double AMCH=amast_(PAA);                                                
+  double AMNE=amast(PNEUTR);                                              
+  double AMCH=amast(PAA);                                                
   if(AMCH<0.0) AMCH=AMEL;                                   
   if (AMNE<0.0) AMNE=0.0;
   double AMTO =PAA[4-j]+PNEUTR[4-j];
