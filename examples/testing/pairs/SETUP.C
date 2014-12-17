b79203073a188073c2ce44a6d5cc934b7fd16350
Zbigniew Was <z.was@cern.ch>
2014-12-17 20:08:11 +0000

    better SETUP.C

--- examples/testing/pairs/SETUP.C
+++ examples/testing/pairs/SETUP.C
@@ -9,6 +9,7 @@
     int n_bins=120;
     double default_min_bin=0.0;
     double default_max_bin=1.1;
+    Setup::mass_power=2;
     Setup::SetHistogramDefaults(n_bins,default_min_bin,default_max_bin);
     Setup::mass_scale_on=true;
     
@@ -23,7 +24,7 @@
     Setup::UTA_params[0]=0.01/91.187; //10 MeV
     // p_t threshold as fraction of particle energy in 
     // mothers frame 
-    Setup::UTA_params[1]=2;
+    Setup::UTA_params[1]=1;
     Setup::UTA_params[2]=0.0;
     Setup::UTA_params[3]=1.0;
     Setup::UTA_params[4]=22;
