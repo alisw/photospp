920445d2bda3edc3610082e1962955e0a2a9caeb
Tomasz Przedzinski <tomasz.przediznski@cern.ch>
2015-03-02 14:07:33 +0000

    HEPEVT example updated with options, comments and another test event

--- examples/photos_hepevt_example_interface.cxx
+++ examples/photos_hepevt_example_interface.cxx
@@ -13,6 +13,18 @@ extern "C" {
   void photos_init_()
   {
     Photos::initialize();
+    Photos::forceMassFromEventRecord(11);
+    
+    // Turn on pair emission and turn off photon emission
+    // Note that this example loops over the same event until at least one
+    // particle is added, so uncommenting these two flags can be used
+    // to test if a pair is correctly emitted
+    //Photos::setPairEmission(true);
+    //Photos::setPhotonEmission(false);
+    
+    // Turn on NLO corrections
+    //Photos::setMeCorrectionWtForW(true);
+    //Photos::setMeCorrectionWtForZ(true);
   }
 
   void photos_process_()
@@ -22,7 +34,17 @@ extern "C" {
     PhotosHEPEVTEvent::read_event_from_HEPEVT(event);
     //event->print();
 
-    event->process();
+    int part = event->getParticleCount();
+    int loop = 0;
+    while( event->getParticleCount() <= part && loop < 10000 ) {
+        event->process();
+        ++loop;
+    }
+    
+    if( loop >= 10000 ) {
+        printf("photos_hepevt_example: there is some technical problem - no particles generated after 10000 loops!\n");
+        exit(-1);
+    }
     //event->print();
 
     PhotosHEPEVTEvent::write_event_to_HEPEVT(event);
